fdc688d30b526947b60f5112f4eb3063f78ff147
Teresa Dratwa-Witek <teresa@iMac-Teresa.local>
2019-03-21 10:20:02 +0100

    rwd tablet

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
-	
+var hamburgerButton = document.querySelector(".hamburger");	
+var menuItems = document.querySelector(".menu ul");
 
+
+hamburgerButton.addEventListener('click', function(){
+
+	if(menuItems.style.display == "inline-block"){
+		menuItems.style.display = "none";
+	}else{
+		menuItems.style.display = "inline-block"
+	}
+
+});
+	
 
 });
